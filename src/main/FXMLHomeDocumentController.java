3b1fb76cd782c1ce6c72014cb42812b0a42bb5d2
Nonato Home <nonato.developer@gmail.com>
2018-11-08 20:57:10 -0300

    initial commit

---
+++ src/main/FXMLHomeDocumentController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.NoSuchObjectException;
+import java.rmi.RemoteException;
+import java.rmi.server.ExportException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author Nonato Dias
+ */
+public class FXMLHomeDocumentController implements Initializable {
+    
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private StackPane dialogStackPane;
+
+    @FXML
+    private JFXTextField textFieldLogin;
+
+    @FXML
+    private JFXTextField textFieldPass;
+
+    @FXML
+    private JFXButton btnSignIn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
